b9b6c4b4d2e3dd71c2c196900c40a12167dbb657
rgiordan <rgiordan@gmail.com>
2014-02-20 15:34:14 -0800

    comments and cleaning.

--- multidimension_rejection_bias_correction.R
+++ multidimension_rejection_bias_correction.R
@@ -1,13 +1,15 @@
 
-library(mvtnorm)
-
-# Note that this dude calculates probabilities within the bounds,
-# not outside.  One could derive what we want from this but it would be
-# complicated.
+library(ggplot2)
 library(tmvtnorm)
 
 setwd("~/Documents/rejection_bias")
 source("~/Documents/rejection_bias/multidimentsion_rejection_bias_correction_lib.R")
+
+# Below, k always denotes the dimension and n the data points in the simulation.
+
+
+#############################
+# Sanity check the area calculations.
 
 k <- 10
 n <- 1e4
@@ -20,56 +22,54 @@ z <- runif(k)
 sigma <- z %*% t(z) + diag(0.1, k)
 sigma.chol <- chol(sigma)
 
-if (F) {
-  # Just sanity check the simulation function
-  x <- SimulateMVNorm(n, rep(2, k), sigma.chol)
-  cov(x) - sigma
-}
-
 SimulatRejectionProb(beta=rep(0, k), sigma.chol=sigma.chol, x.lower=x.lower, x.upper=x.upper)
 SimulatRejectionProb(beta=rep(2, k), sigma.chol=sigma.chol, x.lower=x.lower, x.upper=x.upper)
 SimulatRejectionProb(beta=rep(20, k), sigma.chol=sigma.chol, x.lower=x.lower, x.upper=x.upper)
 
 
-ConditionalLogLikForOptim <- function(beta) {
-  print(beta)
-  return(-ConditionalLogLik(x=beta.hat, beta=beta,
-                            x.lower=beta.hat.lower, x.upper=beta.hat.upper,
-                            sigma=beta.hat.sigma, x.sim=x.sim))
-}
 
-PackageConditionalLogLikForOptim <- function(beta) {
+
+
+
+SimulatedConditionalLogLikForOptim <- function(beta) {
   print(beta)
-  return(-PackageConditionalLogLik(x=beta.hat, beta=beta,
-                                   x.lower=beta.hat.lower, x.upper=beta.hat.upper,
-                                   sigma=beta.hat.sigma))
+  return(-SimulatedConditionalLogLik(x=beta.hat, beta=beta,
+                                     x.lower=beta.hat.lower, x.upper=beta.hat.upper,
+                                     sigma=beta.hat.sigma, x.sim=x.sim))
 }
 
 
-# 1d simulation
+
+##############################
+# 1d simulation with no ground truth.
+
 beta.hat <- 2.2
 beta.hat.lower <- -2
 beta.hat.upper <- 2
-beta.hat.sigma <- sigma <- matrix(0.1)
-sigma.chol <- chol(sigma)
+sigma <- matrix(0.1)
 
-x.sim <- SimulateMVNorm(n=1e4, beta=0, sigma.chol=sigma.chol)
-#x.sim <- rmvnorm(n=1e4, mean=0, sigma=sigma)
-
-1 - PackageAreaCalculation(beta=0, sigma=sigma, x.lower=beta.hat.lower, x.upper=beta.hat.upper)
+x.sim <- rmvnorm(n=1e4, mean=0, sigma=sigma)
+1 - NumericAcceptanceProbability(beta=0, sigma=sigma, x.lower=beta.hat.lower, x.upper=beta.hat.upper)
 SimulatRejectionProb(beta=0, x.sim=x.sim, x.lower=beta.hat.lower, x.upper=beta.hat.upper)
 
 size <- 0.05
-1 - PackageAreaCalculation(beta=0, sigma=sigma, x.lower=-size, x.upper=size)
+1 - NumericAcceptanceProbability(beta=0, sigma=sigma, x.lower=-size, x.upper=size)
 SimulatRejectionProb(beta=0, x.sim=x.sim, x.lower=-size, x.upper=size)
 
+# Note that the simulation method is really slow, even in one dimension,
+# and demonstrably pretty random when <n> is small.  I will not use it from
+# here on out.
+results.simulation <- SimulatedEstimateTruncatedMean(x=beta.hat, sigma=sigma,
+                                                     x.lower=beta.hat.lower,
+                                                     x.upper=beta.hat.upper,
+                                                     n=5e4, verbose=T)
 
-results <- optim(par=0, fn=ConditionalLogLikForOptim,
-                 method="L-BFGS-B", lower=0, upper=beta.hat)
+results.numeric <- NumericEstimateTruncatedMean(x=beta.hat, sigma=sigma,
+                                                x.lower=beta.hat.lower,
+                                                x.upper=beta.hat.upper, verbose=T)
 
-results.package <- optim(par=0, fn=PackageConditionalLogLikForOptim,
-                         method="L-BFGS-B", lower=0, upper=beta.hat)
-
+print(results.simulation$par)
+print(results.numeric$par)
 
 
 
@@ -89,13 +89,13 @@ beta.hat.sigma <- sigma
 x.sim <- rmvnorm(n=1e3, mean=rep(0, 2), sigma=sigma)
 
 size <- 0.05
-1 - PackageAreaCalculation(beta=c(0, 0), sigma=sigma, x.lower=rep(-size, 2), x.upper=rep(size, 2))
+1 - NumericAcceptanceProbability(beta=c(0, 0), sigma=sigma, x.lower=rep(-size, 2), x.upper=rep(size, 2))
 SimulatRejectionProb(beta=c(0, 0), x.sim=x.sim, x.lower=rep(-size, 2), x.upper=rep(size, 2))
 
-results <- optim(par=rep(0, k), fn=ConditionalLogLikForOptim,
+results <- optim(par=rep(0, k), fn=SimulatedConditionalLogLikForOptim,
                  method="L-BFGS-B", lower=0, upper=beta.hat)
 
-results.package <- optim(par=rep(0, k), fn=PackageConditionalLogLikForOptim,
+results.package <- optim(par=rep(0, k), fn=NumericConditionalLogLikForOptim,
                          method="L-BFGS-B", lower=0, upper=beta.hat)
 
 
@@ -114,14 +114,14 @@ sigma.chol <- chol(sigma)
 x.sim <- rmvnorm(n=1e4, mean=rep(0, k), sigma=sigma)
 
 size <- 0.05
-1 - PackageAreaCalculation(beta=rep(0, k), sigma=sigma, x.lower=rep(-size, k), x.upper=rep(size, k))
+1 - NumericAcceptanceProbability(beta=rep(0, k), sigma=sigma, x.lower=rep(-size, k), x.upper=rep(size, k))
 SimulatRejectionProb(beta=rep(0, k), x.sim=x.sim, x.lower=rep(-size, k), x.upper=rep(size, k))
 
 
-results <- optim(par=rep(0, k), fn=ConditionalLogLikForOptim,
+results <- optim(par=rep(0, k), fn=SimulatedConditionalLogLikForOptim,
                  method="L-BFGS-B", lower=0, upper=beta.hat)
 
-results.package <- optim(par=rep(0, k), fn=PackageConditionalLogLikForOptim,
+results.package <- optim(par=rep(0, k), fn=NumericConditionalLogLikForOptim,
                          method="L-BFGS-B", lower=0, upper=beta.hat)
 
 
@@ -149,17 +149,17 @@ beta.hat.lower <- rep(beta.hat.lower.val, k.rej)
 beta.hat.upper <- rep(beta.hat.upper.val, k.rej)
 
 # Much better
-results.package <- optim(par=rep(0, k.rej), fn=PackageConditionalLogLikForOptim,
+results.package <- optim(par=rep(0, k.rej), fn=NumericConditionalLogLikForOptim,
                          method="L-BFGS-B", lower=0, upper=beta.hat)
 
 
 
 
 # Super slow
-results <- optim(par=rep(0, k.rej), fn=ConditionalLogLikForOptim,
+results <- optim(par=rep(0, k.rej), fn=SimulatedConditionalLogLikForOptim,
                  method="L-BFGS-B", lower=rep(0, k.rej), upper=beta.hat)
 
 x.sim <- SimulateMVNorm(n=1e4, beta=rep(0, sum(reject)), sigma.chol=beta.hat.sigma.chol)
-results2 <- optim(par=results$par, fn=ConditionalLogLikForOptim,
+results2 <- optim(par=results$par, fn=SimulatedConditionalLogLikForOptim,
                   method="L-BFGS-B", lower=rep(0, k.rej), upper=beta.hat)
 
--- multidimension_rejection_bias_correction_lib.R
+++ multidimension_rejection_bias_correction_lib.R
@@ -2,54 +2,114 @@
 library(mvtnorm)
 
 
-SimulateMVNorm <- function(n, beta, sigma.chol) {
-  # I do this myself to save the time calculating the cholesky decomposition each
-  # time.
-  k <- length(beta)
-  stopifnot(k == ncol(sigma.chol))
-  x <- matrix(rnorm(n * k), nrow=n)
-  return(x %*% sigma.chol + beta)
-}
-
-SimulatRejectionProb <- function(beta, x.sim, x.lower, x.upper, n=1e4) {
+SimulatRejectionProb <- function(beta, x.sim, x.lower, x.upper) {
+  # For a given beta, estimate the rejection probability. with simulated data.
+  #
+  # Args:
+  #   beta:    A k-vector of the centers of the distribution.
+  #   x.sim:   An n by k matrix of simulations from the desired distribution centered
+  #            at zero.  Here, n should be large to get good simulated results.
+  #   x.lower: A k-vector of lower bounds for rejecting the kth column of <x.sim>.
+  #   x.upper: A k-vector of upper bounds for rejecting the kth column of <x.sim>.
+  #
+  # Returns:
+  #   The proportion of rows of <x.sim> that are outside the bounds specified
+  #   by <x.lower> and <x.upper> when it is recentered to <beta>.  This is an
+  #   estimate of the probability of rejecting data whose covariance structure
+  #   is approximated by <x.sim> is the data were to be centered at <beta>.
+  
   AllRejected <- function(x.row) {
-    all((x.row + beta) < x.lower | (x.row + beta) > x.upper)
+    # A boolean whether or not all the columns of x.row were rejected according
+    # to <x.lower> and <x.upper>, when re-centered at <beta>.
+    return(all((x.row + beta) < x.lower | (x.row + beta) > x.upper))
   }
+  
+  # Sanity check the inputs.
   stopifnot(length(x.lower) == length(beta) && length(x.upper) == length(beta))
   stopifnot(all(x.lower <= x.upper))
-  
-  # Dig that you don't actually need to resimulate x each time -- just recenter it.
+
   z <- apply(x.sim, MARGIN=1, AllRejected) 
   return(sum(z) / length(z))
 }
 
 
 
-ConditionalLogLik <- function(x, x.sim, x.lower, x.upper, beta, sigma) {
+SimulatedConditionalLogLik <- function(x, x.sim, x.lower, x.upper, beta, sigma) {
+  # Use simulated data to return log likelihood conditional on rejection.
+  #
+  # Args:
+  #   x:       The actual observed data (e.g. your regression estimates)
+  #   x.sim:   An n by k matrix of n draws from the covariance structure of 
+  #            <x>, but centered at zero.  This will be used to estimate the
+  #            probability of rejection.  See SimulatRejectionProb for details.
+  #   x.lower: A k-vector of lower bounds for rejecting the kth element of <x>.
+  #   x.upper: A k-vector of upper bounds for rejecting the kth element of <x>.
+  #   beta:    The center of the distribution at which the likelihood is to
+  #            be evaluated (e.g. the candidate true regression parameters).
+  #   sigma:   The covariance matrix of the observations.  Note that <x.sim>
+  #            is expected to be drawn from this covariance structure.
+  #
+  #  Returns:
+  #    An estimated log likelihood of the observations <x>, given that they were drawn
+  #    from a multivariate normal distribution centered at <beta> and with
+  #    covariance <sigma>, conditional on each <x> being outside the bounds
+  #    <x.lower> to <x.upper>.
+  #
+  #   This uses <x.sim> to estimate the probability of being outside the bounds.
+  
+  # The likelihood is zero conditional on an observation not being rejected.
   if (any(x > x.lower & x < x.upper)) {
     return(-Inf)
   }
+  
   l <- dmvnorm(x=x, mean=beta, sigma=sigma, log=TRUE)
   rejection.prob <- SimulatRejectionProb(beta=beta, x.sim=x.sim,
                                          x.lower=x.lower, x.upper=x.upper)
-  log.rejection.prob <- log(rejection.prob + 1e-9)
   
+  # Add 1e-9 to prevent numerical issues.
+  log.rejection.prob <- log(rejection.prob + 1e-9)
   log.lik <- l - log.rejection.prob
-  #print(log.lik)
+  
   return(log.lik)
 }
 
 
-
-
-PackageAreaCalculation <- function(beta, sigma, x.lower, x.upper, t=0.5) {
+NumericAcceptanceProbability <- function(beta, sigma, x.lower, x.upper, t=0.5) {
+  # The probability of at least one multivariate normal draw being within bounds.
+  #
+  # Args:
+  #   beta:    The k-dimensional center of the multivariate normal.
+  #   sigma:   The k by k covariance matrix of the multivariate normal.
+  #   x.lower: A k-vector of lower bounds for rejecting the kth element.
+  #   x.upper: A k-vector of upper bounds for rejecting the kth element.
+  #   t:       Used to select a point within the bounds.  For debugging only.
+  #
+  # Returns:
+  #   A numeric approximation of the probability that a draw from the multivariate
+  #   normal centered at <beta> with covariance <sigma> has at least one component
+  #   between its corresponding <x.lower> and <x.upper>.
+  #
+  #   This function backs this probability out of the full likelihood returned by
+  #   dtmvnorm, which is rather a hack and requires an extra likelihood evaluation.
+  #   It could probably be done more effeciently.
+  #
+  #   Note that this has been observed empirically to have problems for large dimensions
+  #   (like 20 or above).
   
-  # Just need to guarantee that this is within the bounds.
+  # <x> is the point at which we will evaluate the likelihood.  All that matters
+  # is that it be within the bounds.  As long as this is the case, the answer
+  # will not depend on the particular x you choose.
+  stopifnot(t > 0 && t < 1)
   x <- t * x.upper + (1 - t) * x.lower
+  
+  # Each truncated log likelihood will be this value minus the truncation
+  # probability.
   base.log.lik <- dtmvnorm(x=x, mean=beta, sigma=sigma, log=TRUE)
   
   k <- length(beta)
   GetLogArea <- function(i) {
+    # Return the marginal log probabilty of the i_th component lying within
+    # the bounds (x.lower[i], x.upper[i]).
     lower <- rep(-Inf, k)
     upper <- rep(Inf, k)
     lower[i] <- x.lower[i]
@@ -57,37 +117,111 @@ PackageAreaCalculation <- function(beta, sigma, x.lower, x.upper, t=0.5) {
     return(base.log.lik - dtmvnorm(x=x, mean=beta, sigma=sigma,
                                    lower=lower, upper=upper, log=TRUE))
   }
+  
   areas <- exp(sapply(1:k, GetLogArea))
-  #print(areas)
-  #print("--------")
+  
+  # This is the area of the intersection of all the hyper-rectangular strips
+  # of the individual variables.  This area is counted <k>-1 extra times in
+  # <areas>.
   joint.area <-  exp(base.log.lik - dtmvnorm(x=x, mean=beta, sigma=sigma,
                                              lower=x.lower, upper=x.upper, log=TRUE))
-  #print(joint.area)
-  #print("--------")
-  
   total.area <- sum(areas) - (k - 1) * joint.area
   return(total.area)
 }
 
 
-
-PackageConditionalLogLik <- function(x, x.lower, x.upper, beta, sigma) {
+NumericConditionalLogLik <- function(x, x.lower, x.upper, beta, sigma) {
+  # Use numeric area probabilitiess to return log likelihood conditional on rejection.
+  #
+  # Args:
+  #   x:       The actual observed data (e.g. your regression estimates)
+  #   x.lower: A k-vector of lower bounds for rejecting the kth element of <x>.
+  #   x.upper: A k-vector of upper bounds for rejecting the kth element of <x>.
+  #   beta:    The center of the distribution at which the likelihood is to
+  #            be evaluated (e.g. the candidate true regression parameters).
+  #   sigma:   The covariance matrix of the observations.  Note that <x.sim>
+  #            is expected to be drawn from this covariance structure.
+  #
+  #  Returns:
+  #    An estimated log likelihood of the observations <x>, given that they were drawn
+  #    from a multivariate normal distribution centered at <beta> and with
+  #    covariance <sigma>, conditional on each <x> being outside the bounds
+  #    <x.lower> to <x.upper>.
+  #
+  #   This uses NumericAcceptanceProbability to estimate the probability of being
+  #   outside the bounds.  Note that it may have problems in high dimensions (like
+  #   greater than 20).
+  
+  # The likelihood of any observation being within the bounds is zero.
   if (any(x > x.lower & x < x.upper)) {
     return(-Inf)
   }
-  l <- dmvnorm(x=x, mean=beta, sigma=sigma, log=TRUE)
-  rejection.prob <- 1 - PackageAreaCalculation(beta=beta, sigma=sigma,
-                                               x.lower=x.lower, x.upper=x.upper)
-  if (rejection.prob < 0) {
-    print(rejection.prob)
-  }
-  stopifnot(rejection.prob > 0)
   
+  l <- dmvnorm(x=x, mean=beta, sigma=sigma, log=TRUE)
+  rejection.prob <- 1 - NumericAcceptanceProbability(beta=beta, sigma=sigma,
+                                                     x.lower=x.lower, x.upper=x.upper)
+  
+  # This can happen if NumericAcceptanceProbability has numeric issues.
+  stopifnot(rejection.prob > 0 && rejection.prob < 1)
+  
+  # Add 1e-9 to avoid numeric issues.
   log.rejection.prob <- log(rejection.prob + 1e-9)
   
   log.lik <- l - log.rejection.prob
-  #print(log.lik)
   return(log.lik)
 }
 
 
+
+SimulatedEstimateTruncatedMean <- function(x, x.lower, x.upper, sigma,
+                                           verbose=F, n=1e4, ...) {
+  # Use simulation and optim to find the MLE of the center of the truncated distribution.
+  #
+  # Args:
+  #     n:  The number of data points to simulate.  The higher this number is,
+  #         the more accurate the estimated rejection probabilities will be,
+  #         but the slower the estimation will be.
+  #     For other args, see SimulatedConditionalLogLik.
+  #
+  # Returns:
+  #     The output of optim applied to SimulatedConditionalLogLik.
+  
+  x.sim <- rmvnorm(n=n, mean=0, sigma=sigma)
+  SimulatedConditionalLogLikForOptim <- function(beta) {
+    if (verbose) {
+      print(beta)
+    }
+    return(-SimulatedConditionalLogLik(x=x, x.sim=x.sim, beta=beta,
+                                       x.lower=x.lower, x.upper=x.upper,
+                                       sigma=sigma))
+  }
+  
+  results <- optim(par=0.9 * x, fn=SimulatedConditionalLogLikForOptim,
+                   method="L-BFGS-B", lower=rep(0, length(x)), upper=x, ...)
+  return(results) 
+}
+
+
+NumericEstimateTruncatedMean <- function(x, x.lower, x.upper, sigma,
+                                         verbose=F, ...) {
+  # Use optim to find the MLE of the center of the truncated distribution.
+  #
+  # Args:
+  #     See NumericConditionalLogLik.
+  #
+  # Returns:
+  #     The output of optim applied to NumericConditionalLogLik.
+  
+  NumericConditionalLogLikForOptim <- function(beta) {
+    if (verbose) {
+      print(beta)
+    }
+    return(-NumericConditionalLogLik(x=x, beta=beta,
+                                     x.lower=x.lower, x.upper=x.upper,
+                                     sigma=sigma))
+  }
+  
+  results <- optim(par=0.9 * x, fn=NumericConditionalLogLikForOptim,
+                   method="L-BFGS-B", lower=rep(0, length(x)), upper=x, ...)
+  return(results) 
+}
